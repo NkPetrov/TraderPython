cdc421fbc208c6b3a857693d7032a5ed8f0487e4
NVPetrov <lordofgodnd@gmail.com>
2023-03-04 20:32:15 +0300

    add example how use backtrader

---
+++ main/ex2.py
@@ -0,0 +1,17 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import backtrader as bt
+
+class St(bt.Strategy):
+    def __init__(self):
+        self.sma = bt.indicators.SimpleMovingAverage(self.data)
+
+
+data = bt.feeds.BacktraderCSVData(dataname='.\\res\\AAPL.csv')
+
+cerebro = bt.Cerebro()
+cerebro.adddata(data)
+cerebro.addstrategy(St)
+cerebro.run()
+cerebro.plot()
